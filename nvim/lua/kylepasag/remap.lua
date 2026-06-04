8a94cde7ee6c9c30b31ae1e3d154b24b0900a119
Kyle Pasag <kpasag@quadraisland.fas.fa.disney.com>
2026-06-04 16:11:34 -0700

    Added new keybinds for errors, leader e and leader ca

--- nvim/lua/kylepasag/remap.lua
+++ nvim/lua/kylepasag/remap.lua
@@ -3,4 +3,6 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ruler = true
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
