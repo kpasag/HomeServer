99e2fc8996fef64eb48dcbeee074bc17528b5137
Kyle Pasag <kyle.pasag@disneyanimation.com>
2026-06-23 17:35:39 -0700

    Add description for keymaps

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -104,14 +104,12 @@ return {
 },
 {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main",
   build = ":TSUpdate",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
   },
   config = function()
     require("nvim-treesitter.configs").setup({
-      auto_install = true,
       ensure_installed = {
         "bash",
         "c",
@@ -125,6 +123,7 @@ return {
         "typescript",
         "yaml",
       },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
