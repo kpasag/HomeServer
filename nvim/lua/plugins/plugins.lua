13ed05e0317c11b5e4a4928ee9036a93ff841590
Kyle Pasag <kyle.pasag@disneyanimation.com>
2026-07-07 15:18:39 -0700

    Add new plugin

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -233,8 +233,66 @@ return {
   "L3MON4D3/LuaSnip",
   dependencies = { "rafamadriz/friendly-snippets" },
 },
-{ 
+{
   "nvim-tree/nvim-web-devicons", 
-  opts = {} 
+  opts = {}
+},
+{
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+	"            :h-                                  Nhy`               ",
+	"           -mh.                           h.    `Ndho               ",
+	"           hmh+                          oNm.   oNdhh               ",
+	"          `Nmhd`                        /NNmd  /NNhhd               ",
+	"          -NNhhy                      `hMNmmm`+NNdhhh               ",
+	"          .NNmhhs              ```....`..-:/./mNdhhh+               ",
+	"           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
+	"           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
+	"      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
+	" .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
+	" h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
+	" hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
+	" /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
+	"  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
+	"   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
+	"     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
+	"       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
+	"       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
+	"       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
+	"       //+++//++++++////+++///::--                 .::::-------::   ",
+	"       :/++++///////////++++//////.                -:/:----::../-   ",
+	"       -/++++//++///+//////////////               .::::---:::-.+`   ",
+	"       `////////////////////////////:.            --::-----...-/    ",
+	"        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
+	"         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
+	"           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
+	"            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
+	"           s-`::--:::------:////----:---.-:::...-.....`./:          ",
+	"          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
+	"         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
+	"        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
+	"                        .-:mNdhh:.......--::::-`                    ",
+	"                           yNh/..------..`                          ",
+	"                                                                    ",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<CR>"),
+      dashboard.button("g", "  Find text", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("t", "  File tree", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("l", "  Lazy", "<cmd>Lazy<CR>"),
+      dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+    }
+
+    alpha.setup(dashboard.config)
+  end,
 },
 }
